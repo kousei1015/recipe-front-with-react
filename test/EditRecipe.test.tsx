5529f7c5eb28a7dd70dce0666afb7759a3d74884
awata kousei <kousei@awatanoMacBook-Air.local>
2024-05-25 22:58:43 +0900

    test: レシピの編集ページのテストを追加

---
+++ test/EditRecipe.test.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { describe, it, expect, beforeAll, afterEach, afterAll } from "vitest";
+import { render, cleanup, screen } from "@testing-library/react";
+import Index from "../src/routes/$recipeId/edit.lazy";
+import {
+  createRouter,
+  createRootRoute,
+  createRoute,
+  createMemoryHistory,
+  RouterProvider,
+} from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      gcTime: 0,
+    },
+  },
+});
+
+const handlers = [
+  rest.get("http://localhost:3000/v1/recipes/:recipeId.json", (_, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: 1,
+        recipe_name: "test_name",
+        process: "process_test",
+        image_url: null,
+        user_id: 2,
+        user_name: "test_user",
+        avatar_url: null,
+        cooking_time: 2,
+        ingredients: [
+          {
+            name: "test1",
+            quantity: "100g",
+          },
+          {
+            name: "test2",
+            quantity: "100cc",
+          },
+        ],
+      })
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  queryClient.clear();
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => {
+  server.close();
+});
+
+describe("RecipeId Component", () => {
+  it("Firstly, should render skeleton component", async () => {
+    const rootRoute = createRootRoute();
+    const detail = createRoute({
+      getParentRoute: () => rootRoute,
+      path: "/1",
+      component: () => <Index />,
+    });
+    const router = createRouter({
+      routeTree: rootRoute.addChildren([detail]),
+      history: createMemoryHistory({ initialEntries: ["/1"] }),
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+
+    expect(router.state.location.pathname).toBe("/1");
+    await screen.findByText("レシピ編集画面");
+    screen.getByText("材料を追加");
+    await screen.findByDisplayValue("test_name");
+    screen.getByText("process_test");
+    screen.getByDisplayValue("test1");
+    screen.getByDisplayValue("100g");
+    screen.getByDisplayValue("test2");
+    screen.getByDisplayValue("100cc");
+  });
+});
