1b1b5effc1f964743cee92427a1d524e375d8009
awata kousei <kousei@awatanoMacBook-Air.local>
2024-05-30 18:48:36 +0900

    style: レシピの所要時間を表示する背景色を変更

--- src/utils/getBackColorByTime.ts
+++ src/utils/getBackColorByTime.ts
@@ -3,11 +3,11 @@ import { CookingTime } from "../types";
 export const getBackColorByTime = (cookingTime: CookingTime): string => {
   switch (cookingTime) {
     case CookingTime.LessThan5Minutes:
-      return "#9CCC65"; // Green
+      return "#007bff"; // Blue
     case CookingTime.LessThan10Minutes:
-      return "#FFB74D"; // Orange
+      return "#2ca559"; // Bright Green
     case CookingTime.LessThan20Minutes:
-      return "#FFEE58"; // Yellow
+      return "#FFB74D"; // Orange
     case CookingTime.LessThan30Minutes:
       return "#FF8A65"; // Light Red
     case CookingTime.MoreThan30Minutes:
