8bce0ae3a6ecec3451d4766fee6f9549c5edb988
awata kousei <kousei@awatanoMacBook-Air.local>
2024-08-28 17:23:42 +0900

    hotfix: 新規登録の際にPostリクエストを送るURLを修正

--- src/api/postSignUpdata.ts
+++ src/api/postSignUpdata.ts
@@ -2,6 +2,6 @@ import { SignUpProps } from "../types";
 import apiClient from "./apiClient";
 
 export const postSignUpData = async (data: SignUpProps) => {
-  const res = await apiClient.post("/auth/sign_up", data);
+  const res = await apiClient.post("/auth", data);
   return res;
 };
