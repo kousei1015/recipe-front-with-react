a2f4106e2bc1babfa9473d466902298b988e4049
awata kousei <kousei@awatanoMacBook-Air.local>
2024-09-16 14:45:00 +0900

    add: paramsのidのユーザーのフォロー中のユーザー、フォロワーを取得する関数を実行する際、認証情報を加えるように変更
    バックエンドデータに、paramsのidのユーザーのフォロー中のユーザー、フォロワーをログインしているユーザーが既にフォローしているかを判定したフラグを追加したことによる変更

--- src/api/getFollowersByUser.ts
+++ src/api/getFollowersByUser.ts
@@ -1,9 +1,12 @@
 import apiClient from "./apiClient";
 import { FOLLOW } from "../types";
+import { addAuthHeaders } from "./addAuthHeader";
 
 export async function getFollowersByUser(id: string): Promise<FOLLOW> {
   try {
-    const followers = await apiClient.get(`/users/${id}/followers.json`);
+    const followers = await apiClient.get(`/users/${id}/followers.json`, {
+      headers: addAuthHeaders(),
+    });
     return followers.data;
   } catch (error) {
     console.error(error);
